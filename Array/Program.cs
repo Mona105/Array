cb2fe5f86109dba52d6e6311a0ad381b7167e01f
ROY <ROY@DESKTOP-26H23PD>
2022-02-20 18:08:11 +0530

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            bool find = false;
+            int[] arr = { 60, 40, 30, 40 };
+            int search = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (search == arr[i])
+                    find = true;
+                break;
+            }
+            if (find)
+
+            {
+                Console.WriteLine("Found value");
+            }
+            else
+            {
+                Console.WriteLine("Not Match");
+            }
+        }
+    }
+}
